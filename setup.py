64105dfaf2402339dda2dedb8072e67794850d44
Mateo Durante <mdurante@cert.unlp.edu.ar>
2019-04-17 11:39:47 -0300

    parameter rename

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ DEPENDENCIES = open('requirements.txt', 'r').read().split('\n')
 
 setuptools.setup(
     name='pyngen',
-    version='0.1.7',
+    version='0.1.8',
     scripts=['pyngen/ngen'],
     author="CERTUNLP",
     author_email="soporte@cert.unlp.edu.ar",
