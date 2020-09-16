f639243058150f5e97947a5707174756f0b8a03a
mateodurante <mateodurante@gmail.com>
2020-09-16 19:32:16 -0300

    optional create type if not exists

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ DEPENDENCIES = open('requirements.txt', 'r').read().split('\n')
 
 setuptools.setup(
     name='pyngen',
-    version='0.1.30',
+    version='0.1.40',
     scripts=['pyngen/ngen'],
     author="CERTUNLP",
     author_email="soporte@cert.unlp.edu.ar",
